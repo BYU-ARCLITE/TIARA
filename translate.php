08546bd1665092e571f8196a8f1f5abe5a355939
Logan Kearsley <chronosurfer@gmail.com>
2014-02-06 16:15:39 -0700

    Fixed the server-side translation script

--- translate.php
+++ translate.php
@@ -6,17 +6,25 @@ if(!isset($_SESSION['user'])){
 	die;
 }
 
+// For 4.3.0 <= PHP <= 5.4.0
+if (!function_exists('http_response_code')){
+	function http_response_code($code){
+		header("X-PHP-Response-Code: $code", true, $code);
+    }
+}
+
 include "apikeys.php";
 $data = array('srcLang' => $_POST['source'],
               'destLang' => $_POST['target'],
               'word' => $_POST['text']);
-$body = file_get_contents("$translation_endpoint?"+http_build_query($data));
+$body = file_get_contents("$translation_endpoint?".http_build_query($data));
 foreach($http_response_header as $header) {
-    if ('HTTP/' === substr($header, 0, 5)) {
-        list($version, $code, $phrase) = explode(' ', $header, 3);
+	if ('HTTP/' === substr($header, 0, 5)) {
+		list($version, $code, $phrase) = explode(' ', $header, 3);
 		http_response_code((int) $code);
 	} else {
 		header($header);
 	}
 }
 echo $body;
+?>
